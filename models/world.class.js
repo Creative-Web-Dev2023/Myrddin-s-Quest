60a5caf94bf2c769b3a3f07dc40511ed48d41648
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2024-09-15 15:38:46 +0200

    requestAnimationFrame

--- models/world.class.js
+++ models/world.class.js
@@ -1,18 +1,29 @@
-class World{
-    character = new Character();
-    enemies = [
-       new Knight(),
-       new Knight(),
-       new Knight(),
-       
-     ];
-    ctx;
-    constructor(canvas){
-      this.ctx = canvas.getContext('2d');
-      this.draw(); 
-    }
+class World {
+  character = new Character();
+  enemies = [new Knight(),
+             new Knight(), 
+             new Knight()];
+  canvas;
+  ctx;
 
-    draw(){
-      this.ctx.drawImage(this.character.img, this.character.x, this.character.y,this.character.width,this.character.height); 
-    }
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    
+    this.ctx.drawImage(this.character.img,this.character.x,this.character.y,this.character.width,this.character.height);
+    this.enemies.forEach (enemy => {
+      this.ctx.drawImage( enemy.img, enemy.x, enemy.y,enemy.width,enemy.height);
+    });  
+
+    //Draw wird immer wieder aufgerufen
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
 }
