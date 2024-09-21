a029dc006fb1b458e358f0912de01962da509fb2
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2024-09-21 14:18:54 +0200

    Foto character

--- models/character.class.js
+++ models/character.class.js
@@ -5,8 +5,24 @@ class Character extends MovableObject{
      y = 147;
     constructor(){
         super().loadImage('img/wizard/walk/walk_000.png');
-      
+        this.loadImages(['img/wizard/walk/walk_001.png',
+                            'img/wizard/walk/walk_002.png',
+                            'img/wizard/walk/walk_003.png',
+                            'img/wizard/walk/walk_004.png',
+                            'img/wizard/walk/walk_005.png',
+                            'img/wizard/walk/walk_006.png',
+                            'img/wizard/walk/walk_007.png',
+                            'img/wizard/walk/walk_008.png',
+                            'img/wizard/walk/walk_009.png'
+                        ]); 
+        this.animate();
     }
+    animate(){
+        setInterval(() => {
+            this.moveRight();
+        }, 1000/16);
+    }
+    
     jump(){
         
     }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,11 +4,33 @@ class MovableObject {
   img;
   height = 150;
   width = 280;
+  imageCache = {}; //leeres Objekt
 
   //loadImage ('img/test.png');
   loadImage(path) {
     this.img = new Image(); // ist das gleiche wie this.img=document.createElement('img')
     this.img.src = path;
+  }
+
+  /**
+   *
+   * @param {Array} arr -['img/wizard/walk/walk_001.png',
+   *                      'img/wizard/walk/walk_002.png',
+   *                      'img/wizard/walk/walk_003.png',
+   *                      'img/wizard/walk/walk_004.png',
+   *                      'img/wizard/walk/walk_005.png',
+   *                      'img/wizard/walk/walk_006.png',
+   *                      'img/wizard/walk/walk_007.png',
+   *                      'img/wizard/walk/walk_008.png',
+   *                      'img/wizard/walk/walk_009.png',]
+   */              
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;
+    });
   }
   moveRight() {
     console.log("Moving right");
